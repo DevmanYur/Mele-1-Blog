117a50e89664f697339e41daf8b41aecbb817bd8
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-08-29 17:41:11 +0300

    Создание системы комментариев

--- app_blog/models.py
+++ app_blog/models.py
@@ -158,6 +158,48 @@ class Post(models.Model):
                        self.publish.day,
                        self.slug])
 
+    # Это модель Comment. Поле ForeignKey было добавлено для того, чтобы свя-
+    # зать каждый комментарий с одним постом. Указанная взаимосвязь многие-
+    # к-одному определена в модели Comment, потому что каждый комментарий
+    # будет делаться к одному посту, и каждый пост может содержать несколько комментариев.
+    # Атрибут related_name позволяет назначать имя атрибуту, который используется
+    # для связи от ассоциированного объекта назад к нему.
+    # Пост комментарного объекта можно извлекать посредством comment.post
+    # и все комментарии, ассоциированные с объектом-постом, – посредством post.comments.all().
+    # Если атрибут related_name не определен, то Django будет использовать
+    # имя модели в нижнем регистре, за которым следует _set (то есть comment_set),
+    # чтобы именовать взаимосвязь ассоциированного объекта с объектом модели,
+    # в которой эта взаимосвязь была определена.
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+
+    # Мы определили поле created, чтобы хранить дату и время создания ком-
+    # ментария. Используя auto_now_add, дата будет сохраняться автоматически при создании объекта.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # Мы определили булево поле active, чтобы управлять статусом комментариев.
+    # Данное поле позволит деактивировать неуместные комментарии вручную с помощью
+    # сайта администрирования. Мы используем параметр default=True,
+    # чтобы указать, что по умолчанию все комментарии активны.
+    active = models.BooleanField(default=True)
+
+    # В Meta-класс модели был добавлен атрибут ordering = ['created'], чтобы по умолчанию сортировать комментарии
+    # в хронологическом порядке и индексировать поля created в возрастающем порядке.
+    class Meta:
+        ordering = ['created']
+
+    indexes = [
+        models.Index(fields=['created']),
+    ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+
 
 '''
 # 
